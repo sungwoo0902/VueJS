4c534ede77ebfbafe57104b15e44a3b696605b8f
sungwoo0902 <rhtjddn2@gmail.com>
2023-11-08 11:07:04 +0900

    컴포넌트 실습하기

---
+++ ch03/src/components/EmitComponent1.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: "EmitComponent1",
+    props: ["name", "age"],
+    data() {
+        return {
+            myname: this.name,
+            myage: this.age,
+        };
+    },
+    methods: {
+        btnEmit: function() {
+            // alert("click!");
+
+            const emitData = {
+                name: this.myname,
+                age: this.myage,
+            };
+
+            // 상위 컴포넌트로 myEmit 이벤트 발생
+            this.$emit("myEnit", emitData);
+        },
+    },
+};
+</script>
+
+<template>
+    <h4>EmitComponent</h4>
+    <p>
+        이름 : <input type="text" v-model="myname"><br>
+        이름 : <input type="text" v-model="myage"><br>
+    </p>
+    <button @click="btnEmit">전달</button>
+</template>
+
+<style scoped></style>
