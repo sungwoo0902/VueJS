14239cf4c1f9dc9c13462c9954fdac49952369a0
sungwoo0902 <rhtjddn2@gmail.com>
2023-11-13 11:08:43 +0900

    pinia 실습

---
+++ ch06/src/components/MyComponent1.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from "@vue/reactivity";
+import { useMainStore } from "../store/mainStore";
+
+// 전역스토어 가져오기
+const mainStore = useMainStore();
+
+// 전역스토어 getters 실행과 computed를 사용하는 이유
+const count1 = mainStore.doubleCount;
+const count2 = computed(() => mainStore.doubleCount);
+
+// 전역스토어 actions 실행
+const btnIncrement = () => {
+  mainStore.increment();
+};
+
+const btnDecrement = () => {
+  mainStore.decrement();
+};
+</script>
+
+<template>
+  <h4>MyComponent1</h4>
+
+  <p>
+    mainStore count1 : {{ count1 }}<br />
+    mainStore count2 : {{ count2 }}<br />
+  </p>
+  <button @click="btnIncrement">증가</button>
+  <button @click="btnDecrement">감소</button>
+</template>
+
+<style scoped></style>
